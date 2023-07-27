a76ee36303a1410b2f8ba9144ff515ea7b3b6307
Patck63 <85246779+Patck63@users.noreply.github.com>
2023-07-27 08:35:18 +0800

    fix: demo related few shots example

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -16,7 +16,6 @@ from core.modelhelper import get_token_limit
 # AZURE_OPENAI_GPT_DEPLOYMENT = gpt_deployment = davinci          >>>> query completion
 
 
-
 class ChatReadRetrieveReadApproach(Approach):
     # Chat roles
     SYSTEM = "system"
@@ -69,10 +68,10 @@ Question:
 Search query:
 """
     query_prompt_few_shots = [
-        {'role' : USER, 'content' : 'What are my health plans?' },
-        {'role' : ASSISTANT, 'content' : 'Show available health plans' },
-        {'role' : USER, 'content' : 'does my plan cover cardio?' },
-        {'role' : ASSISTANT, 'content' : 'Health plan cardio coverage' }
+        {'role': USER, 'content': '大腸癌有什麼症狀？'},
+        {'role': ASSISTANT, 'content': '大腸癌 症狀'},
+        {'role': USER, 'content': '電療有什麼副作用？'},
+        {'role': ASSISTANT, 'content': '電療 副作用'}
     ]
 
     def __init__(self, search_client: SearchClient, chatgpt_deployment: str, chatgpt_model: str, embedding_deployment: str, sourcepage_field: str, content_field: str):
@@ -86,8 +85,10 @@ Search query:
 
     def run(self, history: Sequence[dict[str, str]], overrides: dict[str, Any]) -> Any:
         has_text = overrides.get("retrieval_mode") in ["text", "hybrid", None]
-        has_vector = overrides.get("retrieval_mode") in ["vectors", "hybrid", None]
-        use_semantic_captions = True if overrides.get("semantic_captions") and has_text else False
+        has_vector = overrides.get("retrieval_mode") in [
+            "vectors", "hybrid", None]
+        use_semantic_captions = True if overrides.get(
+            "semantic_captions") and has_text else False
         top = overrides.get("top") or 3
         exclude_category = overrides.get("exclude_category") or None
         filter = "category ne '{}'".format(
@@ -103,25 +104,27 @@ Search query:
             user_q,
             self.query_prompt_few_shots,
             self.chatgpt_token_limit - len(user_q)
-            )
+        )
 
         chat_completion = openai.ChatCompletion.create(
             deployment_id=self.chatgpt_deployment,
             model=self.chatgpt_model,
-            messages=messages, 
-            temperature=0.0, 
-            max_tokens=32, 
+            messages=messages,
+            temperature=0.0,
+            max_tokens=32,
             n=1)
-        
+
         query_text = chat_completion.choices[0].message.content
         if query_text.strip() == "0":
-            query_text = history[-1]["user"] # Use the last user input if we failed to generate a better query
+            # Use the last user input if we failed to generate a better query
+            query_text = history[-1]["user"]
 
         # STEP 2: Retrieve relevant documents from the search index with the GPT optimized query
 
         # If retrieval mode includes vectors, compute an embedding for the query
         if has_vector:
-            query_vector = openai.Embedding.create(engine=self.embedding_deployment, input=query_text)["data"][0]["embedding"]
+            query_vector = openai.Embedding.create(
+                engine=self.embedding_deployment, input=query_text)["data"][0]["embedding"]
         else:
             query_vector = None
 
@@ -131,52 +134,56 @@ Search query:
 
         # Use semantic L2 reranker if requested and if retrieval mode is text or hybrid (vectors + text)
         if overrides.get("semantic_ranker") and has_text:
-            r = self.search_client.search(query_text, 
-                                        filter=filter,
-                                        query_type=QueryType.SEMANTIC, 
-                                        query_language="en-us", 
-                                        query_speller="lexicon", 
-                                        semantic_configuration_name="default", 
-                                        top=top, 
-                                        query_caption="extractive|highlight-false" if use_semantic_captions else None,
-                                        vector=query_vector, 
-                                        top_k=50 if query_vector else None,
-                                        vector_fields="embedding" if query_vector else None)
+            r = self.search_client.search(query_text,
+                                          filter=filter,
+                                          query_type=QueryType.SEMANTIC,
+                                          query_language="en-us",
+                                          query_speller="lexicon",
+                                          semantic_configuration_name="default",
+                                          top=top,
+                                          query_caption="extractive|highlight-false" if use_semantic_captions else None,
+                                          vector=query_vector,
+                                          top_k=50 if query_vector else None,
+                                          vector_fields="embedding" if query_vector else None)
         else:
-            r = self.search_client.search(query_text, 
-                                        filter=filter, 
-                                        top=top, 
-                                        vector=query_vector, 
-                                        top_k=50 if query_vector else None, 
-                                        vector_fields="embedding" if query_vector else None)
+            r = self.search_client.search(query_text,
+                                          filter=filter,
+                                          top=top,
+                                          vector=query_vector,
+                                          top_k=50 if query_vector else None,
+                                          vector_fields="embedding" if query_vector else None)
         if use_semantic_captions:
             results = [doc[self.sourcepage_field] + ": " + nonewlines(
                 " . ".join([c.text for c in doc['@search.captions']])) for doc in r]
         else:
             # filter r to only include results with a score of 6 or higher
-            r = [doc for doc in r if doc["@search.score"] >= 6]
-            # scores = [doc["@search.score"] for doc in r]
+            # r = [doc for doc in r if doc["@search.score"] >= 6]
+            # scores = [doc for doc in r]
             results = [doc[self.sourcepage_field] + ": " +
-                    nonewlines(doc[self.content_field]) for doc in r]
+                       nonewlines(doc[self.content_field]) for doc in r]
         # print("search result", results)
         # print("=======")
         # print("scores", scores)
         # print("=======")
         content = "\n".join(results)
 
-        follow_up_questions_prompt = self.follow_up_questions_prompt_content if overrides.get("suggest_followup_questions") else ""
-        
+        follow_up_questions_prompt = self.follow_up_questions_prompt_content if overrides.get(
+            "suggest_followup_questions") else ""
+
         # STEP 3: Generate a contextual and content specific answer using the search results and chat history
 
         # Allow client to replace the entire prompt, or to inject into the exiting prompt using >>>
         prompt_override = overrides.get("prompt_override")
         if prompt_override is None:
-            system_message = self.system_message_chat_conversation.format(injected_prompt="", follow_up_questions_prompt=follow_up_questions_prompt)
+            system_message = self.system_message_chat_conversation.format(
+                injected_prompt="", follow_up_questions_prompt=follow_up_questions_prompt)
         elif prompt_override.startswith(">>>"):
-            system_message = self.system_message_chat_conversation.format(injected_prompt=prompt_override[3:] + "\n", follow_up_questions_prompt=follow_up_questions_prompt)
+            system_message = self.system_message_chat_conversation.format(
+                injected_prompt=prompt_override[3:] + "\n", follow_up_questions_prompt=follow_up_questions_prompt)
         else:
-            system_message = prompt_override.format(follow_up_questions_prompt=follow_up_questions_prompt)
-        
+            system_message = prompt_override.format(
+                follow_up_questions_prompt=follow_up_questions_prompt)
+
         messages = self.get_messages_from_history(
             system_message + "\n\nSources:\n" + content,
             self.chatgpt_model,
@@ -197,25 +204,29 @@ Search query:
         msg_to_display = '\n\n'.join([str(message) for message in messages])
 
         return {"data_points": results, "answer": chat_content, "thoughts": f"Searched for:<br>{query_text}<br><br>Conversations:<br>" + msg_to_display.replace('\n', '<br>')}
-    
-    def get_messages_from_history(self, system_prompt: str, model_id: str, history: Sequence[dict[str, str]], user_conv: str, few_shots = [], max_tokens: int = 4096) -> []:
+
+    def get_messages_from_history(self, system_prompt: str, model_id: str, history: Sequence[dict[str, str]], user_conv: str, few_shots=[], max_tokens: int = 4096) -> []:
         message_builder = MessageBuilder(system_prompt, model_id)
 
         # Add examples to show the chat what responses we want. It will try to mimic any responses and make sure they match the rules laid out in the system message.
         for shot in few_shots:
-            message_builder.append_message(shot.get('role'), shot.get('content'))
+            message_builder.append_message(
+                shot.get('role'), shot.get('content'))
 
         user_content = user_conv
         append_index = len(few_shots) + 1
 
-        message_builder.append_message(self.USER, user_content, index=append_index)
+        message_builder.append_message(
+            self.USER, user_content, index=append_index)
 
         for h in reversed(history[:-1]):
             if h.get("bot"):
-                message_builder.append_message(self.ASSISTANT, h.get('bot'), index=append_index)
-            message_builder.append_message(self.USER, h.get('user'), index=append_index)
+                message_builder.append_message(
+                    self.ASSISTANT, h.get('bot'), index=append_index)
+            message_builder.append_message(
+                self.USER, h.get('user'), index=append_index)
             if message_builder.token_length > max_tokens:
                 break
-        
+
         messages = message_builder.messages
         return messages
